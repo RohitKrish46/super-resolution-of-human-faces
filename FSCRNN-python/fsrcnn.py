11e3b2b5d15596949fe3954fad1100b56657380e
Rohit Krishnan <krishr.somu@gmail.com>
2021-05-06 12:32:20 -0700

    Update fsrcnn.py

--- FSCRNN-python/fsrcnn.py
+++ FSCRNN-python/fsrcnn.py
@@ -14,12 +14,12 @@ def model(x, y, lr_size, scale, batch, lr ,d, s, m):
     PS = channels * (scale*scale) # for sub-pixel, PS = Phase Shift
     bias_initializer = tf.constant_initializer(value=0.0)
 
-    # -- Filters and Biases
+    
     filters = [
-        tf.Variable(tf.random_normal([5, 5, 1, d], stddev=0.1), name="f1"),           # (f1,n1,c1) = (5,64,1)
-        tf.Variable(tf.random_normal([1, 1, d, s], stddev=0.1), name="f2"),           # (f2,n2,c2) = (3,12,56)
-        tf.Variable(tf.random_normal([1, 1, s, d], stddev=0.1), name="f%d" % (3 + m)),# (f4,n4,c4) = (1,56,12)
-        tf.Variable(tf.random_normal([1, 1, d, PS], stddev=0.1), name="f%d" % (4 + m)) # (f5,n5,c5) = (9,1,56) 
+        tf.Variable(tf.random_normal([5, 5, 1, d], stddev=0.1), name="f1"),           
+        tf.Variable(tf.random_normal([1, 1, d, s], stddev=0.1), name="f2"),           
+        tf.Variable(tf.random_normal([1, 1, s, d], stddev=0.1), name="f%d" % (3 + m)),
+        tf.Variable(tf.random_normal([1, 1, d, PS], stddev=0.1), name="f%d" % (4 + m)) 
     ]
     bias = [
         tf.get_variable(shape=[d], initializer=bias_initializer, name="b1"),
@@ -29,38 +29,34 @@ def model(x, y, lr_size, scale, batch, lr ,d, s, m):
     ]
     # add filters and biases for 'non-linear mapping' layers (depeding on m), and name them in order
     for i in range(0,m):
-        filters.insert(i+2, tf.Variable(tf.random_normal([3, 3, s, s], stddev=0.1), name="f%d" % (3+i)))  # (f3,n3,c3) = (3,12,12)
+        filters.insert(i+2, tf.Variable(tf.random_normal([3, 3, s, s], stddev=0.1), name="f%d" % (3+i)))  
         bias.insert(i+2, tf.get_variable(shape=[s], initializer=bias_initializer, name="b%d" % (3+i)))
 
-    # -- Model architecture --
-    # feature extraction
+    #Model architecture
+    
     x = tf.nn.conv2d(x, filters[0], [1, 1, 1, 1], padding='SAME', name="conv1")
     x = x + bias[0]
     x = prelu(x, "alpha1")
   
-    # shrinking
+    
     x = tf.nn.conv2d(x, filters[1], [1, 1, 1, 1], padding='SAME', name="conv2")
     x = x + bias[1]
     x = prelu(x, "alpha2")
   
-    # non-linear mapping (amount of layers depends on m)
+    
     for i in range(0,m):
         x = tf.nn.conv2d(x, filters[2+i], [1, 1, 1, 1], padding='SAME', name="conv%d" % (3+i))
         x = x + bias[2+i]
         x = prelu(x, "alpha{}".format(3+i))
   
-    # expanding
+    
     x = tf.nn.conv2d(x, filters[3+(m-1)], [1, 1, 1, 1], padding='SAME', name="conv%d" % (3+m))
     x = x + bias[3+(m-1)]
     x = prelu(x, "alpha{}".format(3+m))
     
     x = tf.nn.conv2d(x, filters[4+(m-1)], [1, 1, 1, 1], padding='SAME', name="conv%d" % (4+m))
 
-    # transpose
-    # x = tf.nn.conv2d_transpose(x, filters[4+(m-1)], output_shape=[batch,lr_size*scale,lr_size*scale,1],
-    #                                                 strides=[1, scale, scale, 1], 
-    #                                                 padding='SAME', 
-    #                                                 name="deconv")
+    
 
     # sub-pixel
     x = tf.nn.depth_to_space(x, scale, data_format='NHWC')
